2bd59d4ad3d5a669305876c1cb22390984a1eac5
rafael <rafaelcompp@gmail.com>
2024-08-20 20:05:18 -0300

    gravando codigos

---
+++ src/main/java/com/mycompany/calculadoraroubada/Calculadora.java
@@ -0,0 +1,21 @@
+
+public class Calculadora {
+
+	public Integer somar(Integer a, Integer b){
+		return a + b;
+	}
+
+	public Integer multiplicar(Integer a, Integer b) {
+		return a * b;
+	}
+
+	public Integer subtrair(Integer a, Integer b) {
+		return b - a;
+	}
+	
+	public Integer dividir(Integer a, Integer b){
+		return a / b;
+	}
+	
+
+}
